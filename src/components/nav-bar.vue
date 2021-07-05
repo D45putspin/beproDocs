cbc66a40b816551aedc99a33a02ece8b2b67852a
D45putspin <t14g0@protonmail.com>
2021-07-05 17:00:57 +0100

    b&wMode

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -5,6 +5,9 @@
       <span class="ml-2 align-self-start subtitle-small">//docs</span>
     </router-link>
     <div>
+      <button class="btn-success mr-4" id="theme-switcher">Switch Mode</button>
+    </div>
+    <div>
       <a href="https://github.com/bepronetwork/bepro-js">github/bepro-js</a>
     </div>
   </div>
@@ -15,6 +18,27 @@ import { Component, Emit, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component
 export default class NavBar extends Vue {}
+ // Wait for document to load
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "light");
+
+    // Get our button switcher
+    var themeSwitcher = document.getElementById("theme-switcher");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function() {
+      // Get the current selected theme, on the first run
+      // it should be `light`
+      var currentTheme = document.documentElement.getAttribute("data-theme");
+
+      // Switch between `dark` and `light`
+      var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+      // Set our currenet theme to the new one
+      document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+  });
+
 </script>
 
 <style scoped lang="scss">
